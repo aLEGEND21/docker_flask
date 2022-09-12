2470e58be03f2277ac4381e82d1c490e0bbfd7a1
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-12 18:45:13 -0400

    Switch to using git clone to retrieve copy of the source code

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,9 @@
 echo killing old docker processes
 sudo docker-compose rm -fs
 
+echo copying .env file
+cp .env bot/.env
+cp .env flask_app/.env
+
 echo building docker containers
 sudo docker-compose up --build -d
