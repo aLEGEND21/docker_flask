b6128cb88071a229500c7952077a7bc8ebe9ece3
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-12 18:48:45 -0400

    Change .env path to relative path

--- run_docker.sh
+++ run_docker.sh
@@ -2,8 +2,8 @@ echo killing old docker processes
 sudo docker-compose rm -fs
 
 echo copying .env file
-cp .env bot/.env
-cp .env flask_app/.env
+cp ./.env ./bot/.env
+cp ./.env ./flask_app/.env
 
 echo building docker containers
 sudo docker-compose up --build -d
