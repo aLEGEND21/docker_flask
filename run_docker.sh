769521ecc8638d350161b942d8bfe8c4684f37df
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-13 18:55:29 -0400

    Switch gunicorn to port 5000

--- run_docker.sh
+++ run_docker.sh
@@ -5,4 +5,4 @@ echo updating docker images
 docker pull alegend217/docker_flask
 
 echo running docker container
-docker run -d -p 5000:5000 -p 80:80 alegend217/docker_flask
+docker run -d -p 8000:8000 -p 80:80 alegend217/docker_flask
