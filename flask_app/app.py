2c7dd0d6db8fd0e50a4a411890be8b26dd714b95
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-15 20:28:04 -0400

    Undo previous commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,14 @@
 import flask
 import os
 from dotenv import load_dotenv
-#from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 load_dotenv()
 
 server = flask.Flask(__name__)
-#server.wsgi_app = ProxyFix(
-#    server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-#)
+server.wsgi_app = ProxyFix(
+    server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 @server.route('/')
 def hello_world():
