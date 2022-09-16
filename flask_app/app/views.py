5153b9800a8c64b355fb9d4b5d0bc35098d4b1f2
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-16 15:54:43 -0400

    Remove webhooks and update update.sh to not overwrite the .env file

--- flask_app/app/views.py
+++ flask_app/app/views.py
@@ -1,7 +1,8 @@
 import flask
+import os
 
 views = flask.Blueprint('views', __name__)
 
 @views.route("/")
 def home():
-    return "Hello There!"
+    return f"SECRET_KEY: {os.environ.get('SECRET_KEY')}"
--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 # This should only be run directly and located one directory above the project
+env=`cat docker_flask/.env` # Store the .env file for later
 rm -rf docker_flask
 git clone https://github.com/aLEGEND21/docker_flask
 cd docker_flask
+echo $env > .env # Copy the old .env file to the updated project
 bash run.sh
