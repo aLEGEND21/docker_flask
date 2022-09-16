96e1c828a02035f34064a50099b50c96e83877a0
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-16 15:44:24 -0400

    Attempt to use a github webhook to update the scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 echo killing old docker processes
 sudo docker-compose rm -fs
 
-echo copying .env file
-cp ./.env ./bot/.env
-cp ./.env ./flask_app/.env
-
 echo building docker containers
 sudo docker-compose up --build -d
---
+++ update.sh
@@ -0,0 +1,3 @@
+cd ..
+rm -rf docker_flask
+git clone https://github.com/aLEGEND21/docker_flask
