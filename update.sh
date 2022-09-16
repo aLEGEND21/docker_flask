2fa7cbb0833b71b46af4221a0d9a173549bd2b89
aLEGEND21 <arnavmurthi@gmail.com>
2022-09-16 15:49:28 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
-cd ..
+# This should only be run directly and located one directory above the project
 rm -rf docker_flask
 git clone https://github.com/aLEGEND21/docker_flask
+cd docker_flask
+bash run.sh
